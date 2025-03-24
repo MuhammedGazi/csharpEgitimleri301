8724adf0c5cbd7821f29c0fb6086d518f5ed3159
MuhammedGazi <muhammed.celenk1@ogr.sakarya.edu.tr>
2025-03-25 02:11:10 +0300

    katmanlı mimari

---
+++ csharpEgitimleri301.EntityLayer/Concrete/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpEgitimleri301.EntityLayer.Concrete
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int ProductStock { get; set; }
+
+        public decimal ProductPrice { get; set; }
+
+        public string ProductDescription { get; set; }
+    }
+}
